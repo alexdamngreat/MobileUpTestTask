6544a497371fe88faeb1af497c68f9a61ed8d7ed
timofeev <alexander.timofeev.v@gmail.com>
2021-07-21 18:00:57 +0300

    Initial Commit

---
+++ MobileUpTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MobileUpTestTask
+//
+//  Created by Александр Тимофеев on 21.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
